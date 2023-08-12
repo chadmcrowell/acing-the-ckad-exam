4917815c076d39314b40cc892742773bd50b9409
Chad Crowell <chadmcrowell@gmail.com>
2023-08-12 11:31:10 -0500

    added  to nfs-helper script

--- ch_02/nfs-helper.sh
+++ ch_02/nfs-helper.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo apt install nfs-common
+sudo apt update && sudo apt install nfs-common
+
+# to mount this from another server (specific to killercoda)
+
+sudo mkdir /var/data
+
+sudo mount -t nfs -o nfsvers=3 172.30.2.2:/data /var/data
