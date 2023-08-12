1a572ae6f584cf2781a6c5eff056a66d1bf73196
Chad Crowell <chadmcrowell@gmail.com>
2023-08-12 11:27:49 -0500

    changed nfs-server-install mounting options

--- ch_02/nfs-server-install.sh
+++ ch_02/nfs-server-install.sh
@@ -7,9 +7,28 @@ apt install -y nfs-server
 mkdir /data
 
 cat << EOF >> /etc/exports
-/data 172.30.2.2(rw,no_subtree_check,no_root_squash)
+/data *(rw,no_subtree_check,no_root_squash,insecure)
 EOF
+
+# /data *(rw,sync,no_root_squash,insecure)
+# /data *(rw,no_subtree_check,no_root_squash,insecure)
+
+# This was the original
+# /data 172.30.2.2(rw,no_subtree_check,no_root_squash) 
 
 systemctl enable --now nfs-server
 
 exportfs -ar
+
+# exportfs -rav
+
+
+
+# to mount this from another server (specific to killercoda)
+
+# sudo apt update && sudo apt install nfs-common
+
+# sudo mkdir /var/data
+
+# sudo mount -t nfs -o nfsvers=3 172.30.2.2:/data /var/data
+
